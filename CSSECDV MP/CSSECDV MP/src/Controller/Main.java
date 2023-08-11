1725969f26e91424850a17131b95691a9536ee1c
DLSUliogarcia <aurelio_garcia@dlsu.edu.ph>
2023-08-12 02:55:09 +0800

    Additional Changes

--- CSSECDV MP/CSSECDV MP/src/Controller/Main.java
+++ CSSECDV MP/CSSECDV MP/src/Controller/Main.java
@@ -40,9 +40,9 @@ public class Main {
 //        sqlite.createUserTable();
 //        
 //        // Add sample history
-//        sqlite.addHistory("admin", "Antivirus", 1, "2019-04-03 14:30:00.000");
-//        sqlite.addHistory("manager", "Firewall", 1, "2019-04-03 14:30:01.000");
-//        sqlite.addHistory("staff", "Scanner", 1, "2019-04-03 14:30:02.000");
+//        sqlite.addHistory("client1", "Antivirus", 1, "2019-04-03 14:30:00.000");
+//        sqlite.addHistory("client1", "Firewall", 1, "2019-04-03 14:30:01.000");
+//        sqlite.addHistory("client2", "Scanner", 1, "2019-04-03 14:30:02.000");
 //        
 //        // Add sample logs
 //        sqlite.addLogs("NOTICE", "admin", "User creation successful", new Timestamp(new Date().getTime()).toString());
